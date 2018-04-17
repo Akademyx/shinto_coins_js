a6698facefd240b4d6a029b410d3ceade38c92eb
Alex Tran <lxdemikz@gmail.com>
2018-04-17 16:50:56 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var app = express();
+var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+var path = require('path');
+app.use(express.static(__dirname + '/public/dist'));
+// configure body-parser to read JSON
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+var dbURI = 'mongodb://localhost/shinto'
+mongoose.connect(dbURI);
+
+var User = new mongoose.Schema({
+    
+})
+
+app.all("*", (req, res, next) => {
+    res.sendFile(path.resolve("./public/dist/index.html"))
+});
+
+app.listen(8000, () => {
+    console.log("listening on port 8000");
+});
